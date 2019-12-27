2da42f326601d17f4fde5437feac0e71d6b54238
YUHYEMIN <yuhyemin96@gmail.com>
2019-12-27 10:56:38 +0900

    커밋

--- HotelProject/src/notice/model/service/NoticeService.java
+++ HotelProject/src/notice/model/service/NoticeService.java
@@ -1,5 +1,20 @@
 package notice.model.service;
 
+import static common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.List;
+
+import notice.model.dao.NoticeDAO;
+import notice.model.vo.Notice;
+
 public class NoticeService {
+
+	public List<Notice> selectNoticeAll(int cPage, int numPerPage) {
+		Connection conn = getConnection();
+		List<Notice> list = new NoticeDAO().selectNoticeAll(conn, cPage, numPerPage);
+		close(conn);
+		return list;
+	}
 
 }
