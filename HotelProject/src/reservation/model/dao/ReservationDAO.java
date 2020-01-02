1d92e2922229364dcaf3815296a8cf700b899f3a
YUHYEMIN <yuhyemin96@gmail.com>
2020-01-03 02:27:23 +0900

    덜올라갔네

--- HotelProject/src/reservation/model/dao/ReservationDAO.java
+++ HotelProject/src/reservation/model/dao/ReservationDAO.java
@@ -15,6 +15,7 @@ import java.util.Properties;
 
 import coupon.model.vo.Coupon;
 import notice.model.dao.NoticeDAO;
+import reservation.model.vo.BookedRoom;
 import reservation.model.vo.Reservation;
 import room.model.vo.Room;
 
@@ -29,6 +30,7 @@ public class ReservationDAO {
 			e.printStackTrace();
 		}
 	}
+	
 	public List<Room> selectAllRoomList(Connection conn) {
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
@@ -66,6 +68,7 @@ public class ReservationDAO {
 		
 		return list;
 	}
+	
 	//해당 체크인 체크아웃날짜에 예약된 객실(room_no)의 개수
 	public Map<Integer, Integer> selectReservedRoomCntList(Connection conn, String checkindate, String checkoutdate) {
 		PreparedStatement pstmt = null;
@@ -95,6 +98,7 @@ public class ReservationDAO {
 		
 		return map;
 	}
+	//해당 멤버가 결제시에 사용할 수 있는 쿠폰 리스트(사용 최소 금액 고려)
 	public Map<String, Object> selectCouponListByMemberId(Connection conn, String memberId, int totalRoomPrice) {
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
@@ -126,6 +130,7 @@ public class ReservationDAO {
 		
 		return map;
 	}
+	//결제 성공 시에 예약 정보 예약 테이블에 INSERT
 	public int insertReservation(Connection conn, Reservation resv) {
 		PreparedStatement pstmt = null;
 		int result = 0;
@@ -152,6 +157,70 @@ public class ReservationDAO {
 		
 		return result;
 	}
-
+	//현재 예약 번호 가져오기
+	public long selectLastSeq(Connection conn) {
+		long resv_no = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String query = prop.getProperty("selectLastSeq");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			rset = pstmt.executeQuery();
+			if(rset.next()) {
+				resv_no = rset.getLong("currval");
+			}
+			System.out.println("새로발급받은 게시글번호@dao="+resv_no);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return resv_no;
+	}
+	//예약 완료 후 예약된 객실 테이블에 INSERT
+	public int insertBookedRoom(Connection conn, BookedRoom bookedroom) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = prop.getProperty("insertBookedRoom");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1,bookedroom.getBookedRoomNo());
+			pstmt.setLong(2, bookedroom.getBookedResvNo());
+			pstmt.setInt(3, bookedroom.getBookedPeople());
+			pstmt.setDate(4, bookedroom.getBookedCheckIn());
+			pstmt.setDate(5, bookedroom.getBookedCheckOut());
+			pstmt.setInt(6, bookedroom.getBookedPrice());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
 	
+	//결제시에 쿠폰 사용 했을 경우, 해당 쿠폰 상태 변경
+	public int updateCouponStatus(Connection conn, String usedCouponNo) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = prop.getProperty("updateCouponStatus");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, usedCouponNo);
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		return result;
+	}
 }
--- HotelProject/src/reservation/model/service/ReservationService.java
+++ HotelProject/src/reservation/model/service/ReservationService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import reservation.model.dao.ReservationDAO;
+import reservation.model.vo.BookedRoom;
 import reservation.model.vo.Reservation;
 import room.model.vo.Room;
 public class ReservationService {
@@ -19,7 +20,7 @@ public class ReservationService {
 		Map<Integer,Integer> map = new ReservationDAO().selectReservedRoomCntList(conn,checkindate,checkoutdate);
 		List<Room> roomlist = new ReservationDAO().selectAllRoomList(conn);
 		for(Room r : roomlist) {
-			int available = r.getRoomPeople() - map.get(r.getRoomNo());
+			int available = r.getRoomAmount() - map.get(r.getRoomNo());
 			if(available>=roomcnt) {
 				list.add(r);
 			}
@@ -38,11 +39,28 @@ public class ReservationService {
 	public int insertReservation(Reservation resv) {
 		Connection conn = getConnection();
 		int result = new ReservationDAO().insertReservation(conn,resv);
+		if(result>0) {
+			//새로 발급된 예약 번호 가져와서 resv에 대입
+			resv.setResvNo(new ReservationDAO().selectLastSeq(conn));
+			commit(conn);
+		}
+		else commit(conn);
+		return result;
+	}
+
+	public int insertBookedRoom(BookedRoom bookedroom) {
+		Connection conn = getConnection();
+		int result = new ReservationDAO().insertBookedRoom(conn,bookedroom);
 		if(result>0) commit(conn);
 		else commit(conn);
 		return result;
 	}
 
-
-
+	public int updateCouponStatus(String usedCouponNo) {
+		Connection conn = getConnection();
+		int result = new ReservationDAO().updateCouponStatus(conn,usedCouponNo);
+		if(result>0) commit(conn);
+		else commit(conn);
+		return result;
+	}
 }
